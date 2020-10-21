ed9e48f9333922ca53f3d325c3a9759afe0e1014
mattchley <mattchley@gmail.com>
2020-10-21 13:10:32 -0700

    fixed templates and allowed for search on weather app, would like a default user locale to load

--- basicProjects/weather/views.py
+++ basicProjects/weather/views.py
@@ -5,12 +5,15 @@ import requests
 import json
 
 def index(request):
-    location='Stockholm'
+    # find local weather on load
+    if request.method == 'GET':
+
+        search_query = request.GET.get('search_box', "San Diego")
+
+    
+    location=search_query
     api_key=os.getenv("WEATHER")
-
     url="https://api.openweathermap.org/data/2.5/weather?q=%s&units=imperial&APPID=%s" %(location, api_key)
-
-
     response = requests.get(url)
     data = json.loads(response.text)
     print(data)
@@ -21,19 +24,7 @@ def index(request):
         "desc" : data['weather'][0]['description'],
         "icon" : data['weather'][0]['icon'],
     }
-
-    print(city_weather)
-
-    # {'coord': {'lon': 18.06, 'lat': 59.33}, 
-    # 'weather': [{'id': 741, 'main': 'Fog', 'description': 'fog', 'icon': '50n'}], 
-    # 'base': 'stations', 
-    # 'main': {'temp': 37.27, 'feels_like': 33.93, 'temp_min': 33.8, 'temp_max': 41, 'pressure': 1023, 'humidity': 100}, 
-    # 'visibility': 300, 'wind': {'speed': 1.12, 'deg': 300}, 
-    # 'clouds': {'all': 12}, 
-    # 'dt': 1602879701, 
-    # 'sys': {'type': 1, 'id': 1788, 'country': 'SE', 'sunrise': 1602826108, 'sunset': 1602862663}, 
-    # 'timezone': 7200, 'id': 2673730, 'name': 'Stockholm', 'cod': 200}
-  
-    return HttpResponse("Hello, world. You're at the weather index.")
+    context = {'city_weather' : city_weather}
+    return render(request, 'weather/weather.html', context)
     
 # Create your views here.
