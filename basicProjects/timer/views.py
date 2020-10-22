1501a6932ddaaf9b04b4a155e5cd7e1eb50231e5
mattchley <mattchley@gmail.com>
2020-10-22 15:03:46 -0700

    timer function

--- basicProjects/timer/views.py
+++ basicProjects/timer/views.py
@@ -1,9 +1,19 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 import os
+import threading
+
+seconds = 10
+def tester(): 
+    print("it has been %d"%(seconds))
+    
+    
+timer = threading.Timer(seconds, tester) 
+timer.start() 
+print("Exit\n") 
 
 def index(request):
-    print(os.getenv("TEST_KEY"))
+    
     return HttpResponse("Hello, world. You're at the timer index.")
     
 # Create your views here.
