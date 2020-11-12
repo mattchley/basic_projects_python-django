96cff0b5864973ef81b91c33bf42e416551235db
mattchley <mattchley@gmail.com>
2020-11-12 15:47:34 -0800

    limit 5 previous search results in weather app, needs to have better presentation

--- basicProjects/weather/views.py
+++ basicProjects/weather/views.py
@@ -5,6 +5,7 @@ import json
 from .forms import Weather_Form
 # searches for the weather data the user inputs
 # needs a list obj to create a list pf previous serarches
+previous_weather_holder=[]
 def weather_search(location):
     api_key=os.getenv("WEATHER")
     url="https://api.openweathermap.org/data/2.5/weather?q=%s&units=imperial&APPID=%s" %(location, api_key)
@@ -24,14 +25,18 @@ def index(request):
         if form.is_valid():
             location = form.cleaned_data['location']
             city_obj = weather_search(location)
+            previous_weather_holder.append(city_obj)
+            print(previous_weather_holder)
+            previous_list = previous_weather_holder
     else:
         form = Weather_Form()
-        city_obj = None   
+        city_obj = None
+        previous_list = None   
 
     
     
     
-    return render(request, 'weather/weather.html', {'form':form, 'city_obj': city_obj})
+    return render(request, 'weather/weather.html', {'form':form, 'city_obj': city_obj, 'previous_list':previous_list})
     
     
     
